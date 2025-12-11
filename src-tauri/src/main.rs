995ae850303341dc1f6d2f3175f22a54baeb7fbb
unknown <naoki.takahashi1@rohm.co.jp>
2025-12-11 17:56:24 +0900

    251211@rohm

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use tauri::{Emitter};
 use tauri::menu::MenuBuilder;
 use tauri_plugin_dialog::{DialogExt,MessageDialogKind};
